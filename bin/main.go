53531bb8a204d9f0cae365c222b1ff35c2fa1bd4
Jonathan Gersam S. Lopez <jonathangersam@gmail.com>
2022-01-08 11:30:14 +0800

    add main

--- bin/main.go
+++ bin/main.go
@@ -1,1 +1,24 @@
-package bin
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func simpleGreet() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome!"))
+	}
+}
+
+func main() {
+	fmt.Println("Application starting...")
+
+	http.HandleFunc("/", simpleGreet())
+
+	fmt.Println("Listening on port 8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
